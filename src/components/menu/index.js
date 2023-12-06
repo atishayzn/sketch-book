9620ddc220da7cdc07bb0522eb8a64e0c904fa17
atishay jain <jatishay444@gmail.com>
2023-12-07 02:27:43 +0530

    Toolbox and Menu UI completed

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,24 +1,34 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPencil, faEraser, faRotateLeft, faRotateRight, faFileArrowDown } from '@fortawesome/free-solid-svg-icons'
 import styles from './index.module.css'
+import { useDispatch,useSelector } from 'react-redux'
+import { MENU_ITEMS } from '@/constant'
+import { menuItemClick } from '@/slice/menuSlice'
+import cx from 'classnames'
 
 
 const Menu = () =>{
+    const dispatch= useDispatch();
+    const activeMenuItem=useSelector(state=>state.menu.activeMenuItem)
+    const handleMenuClick=(itemName)=>{
+        dispatch(menuItemClick(itemName))
+    }
+
     return(
         <div className={styles.menuContainer}>
-            <div styles={styles.iconWrapper}>
+            <div className={cx(styles.iconWrapper, {[styles.active]: activeMenuItem === MENU_ITEMS.PENCIL})}  onClick={()=>handleMenuClick(MENU_ITEMS.PENCIL)}>
                 <FontAwesomeIcon icon={faPencil} styles={styles.icon}/>
             </div>
-            <div styles={styles.iconWrapper}>
-                <FontAwesomeIcon icon={faEraser} styles={styles.icon} />
+            <div className={cx(styles.iconWrapper, {[styles.active]: activeMenuItem === MENU_ITEMS.ERASER})} onClick={()=>handleMenuClick(MENU_ITEMS.ERASER)}>
+                <FontAwesomeIcon icon={faEraser} styles={styles.icon}  />
             </div>
-            <div styles={styles.iconWrapper}>
+            <div className={styles.iconWrapper}>
                 <FontAwesomeIcon icon={faRotateLeft} styles={styles.icon}  />
             </div>
-            <div styles={styles.iconWrapper}>
+            <div className={styles.iconWrapper} >
                 <FontAwesomeIcon icon={faRotateRight} styles={styles.icon} />
             </div>
-            <div styles={styles.iconWrapper}>
+            <div className={styles.iconWrapper}>
                 <FontAwesomeIcon icon={faFileArrowDown} styles={styles.icon} />
             </div>
 
--- src/components/toolbox/index.js
+++ src/components/toolbox/index.js
@@ -1,27 +1,39 @@
 import styles from './index.module.css'
 import cx from 'classnames';
-import { COLORS, } from '@/constant';
+import { useDispatch, useSelector } from 'react-redux';
+import { MENU_ITEMS,COLORS } from '@/constant';
+import { changeBrushSize, changeColor } from '@/slice/toolboxSlice';
 
 const Toolbox =()=>{
-    const updateBrushSize=()=>{
+    const activeMenuItem=useSelector(state=>state.menu.activeMenuItem)
+    const showStrokeToolOption = activeMenuItem === MENU_ITEMS.PENCIL
+    const showBrushToolOption = activeMenuItem === MENU_ITEMS.PENCIL || activeMenuItem === MENU_ITEMS.ERASER
+    const {color, size} = useSelector((state) => state.toolbox[activeMenuItem])
+    const dispatch=useDispatch()
+    const updateColor=(newColor)=>{
+        dispatch(changeColor({item:activeMenuItem,color:newColor}))
 
     }
+    const updateBrushSize=(e)=>{
+        dispatch(changeBrushSize({item:activeMenuItem,size:e.target.value}))
+    }
+
     return (<div className={styles.toolboxContainer}>
-        {<div className={styles.toolItem}>
+        {showStrokeToolOption && <div className={styles.toolItem}>
             <h4 className={styles.toolText}>Stroke Color</h4>
             <div className={styles.itemContainer}>
-                <div className={styles.colorBox} style={{backgroundColor: COLORS.BLACK}} />
-                <div className={styles.colorBox} style={{backgroundColor: COLORS.RED}} />
-                <div className={styles.colorBox} style={{backgroundColor: COLORS.GREEN}} />
-                <div className={styles.colorBox} style={{backgroundColor: COLORS.BLUE}} />
-                <div className={styles.colorBox} style={{backgroundColor: COLORS.ORANGE}} />
-                <div className={styles.colorBox} style={{backgroundColor: COLORS.YELLOW}} />
+                <div className={cx(styles.colorBox, {[styles.active]: color === COLORS.BLACK})}  style={{backgroundColor: COLORS.BLACK}}  onClick={()=>updateColor(COLORS.BLACK)} />
+                <div className={cx(styles.colorBox, {[styles.active]: color === COLORS.RED})} style={{backgroundColor: COLORS.RED}}  onClick={()=>updateColor(COLORS.RED)}/>
+                <div className={cx(styles.colorBox, {[styles.active]: color === COLORS.GREEN})} style={{backgroundColor: COLORS.GREEN}}  onClick={()=>updateColor(COLORS.GREEN)}/>
+                <div className={cx(styles.colorBox, {[styles.active]: color === COLORS.BLUE})} style={{backgroundColor: COLORS.BLUE}}  onClick={()=>updateColor(COLORS.BLACK)}/>
+                <div className={cx(styles.colorBox, {[styles.active]: color === COLORS.ORANGE})} style={{backgroundColor: COLORS.ORANGE}}  onClick={()=>updateColor(COLORS.ORANGE)}/>
+                <div className={cx(styles.colorBox, {[styles.active]: color === COLORS.YELLOW})} style={{backgroundColor: COLORS.YELLOW}}  onClick={()=>updateColor(COLORS.YELLOW)}/>
             </div>
         </div>}
-        {<div className={styles.toolItem}>
+        {showBrushToolOption && <div className={styles.toolItem}>
             <h4 className={styles.toolText}>Brush Size</h4>
             <div className={styles.itemContainer}>
-                <input type="range" min={1} max={10} step={1} onChange={updateBrushSize} />
+                <input type="range" min={1} max={10} step={1} onChange={updateBrushSize} value={size}/>
             </div>
         </div>}
     </div>)
